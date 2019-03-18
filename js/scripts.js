9522f100d1aad10db047c63b690a167ff20bf800
Aman <amanj9641@gmail.com>
2019-03-18 09:42:53 +0000

    Npm scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+            $('#mycarousel').carousel({interval:2000});
+            $('#carouselButton').click(function(){
+
+                if($('#carouselButton').children('span').hasClass('fa-pause')){
+                    $('#mycarousel').carousel('pause');
+                    $('#carouselButton').children('span').removeClass('fa-pause');
+                    $('#carouselButton').children('span').addClass('fa-play');
+
+                }
+                else 
+                {
+                     $('#mycarousel').carousel('cycle');
+                    $('#carouselButton').children('span').removeClass('fa-play');
+                    $('#carouselButton').children('span').addClass('fa-pause');
+                }
+            });
+           
+           $(".navbar-text a").click(function(){
+                $('#loginModal').modal({
+                  keyboard: false
+                });
+           });
+
+           $(".modal-header button").click(function() {
+                $("#loginModal").modal('hide');
+           });
+           
+
+        });
